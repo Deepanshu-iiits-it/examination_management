1e3122eaa653c858f8362a77e44675054f83a290
Harsh Soni <devilincarcerated020@yahoo.com[credential]>
2022-10-06 23:29:39 +0530

    add validation to subject credit

--- examination_management/subject/models.py
+++ examination_management/subject/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
+from django.core import validators
 
 from examination_management.core.behaviours import StatusMixin
 
 
 class Subject(StatusMixin, TimeStampedModel):
     name = models.CharField(_('Name'), max_length=100, null=True, blank=True)
-    credit = models.IntegerField(_('Credit'), null=True, blank=True)
-    code = models.CharField(_('Code'), max_length=100, unique=True,
-                               null=True, blank=True, db_index=True)
+    credit = models.IntegerField(_('Credit'), null=True, blank=True, validators=[validators.MinValueValidator(0),
+                                                                                 validators.MaxValueValidator(10)])
+    code = models.CharField(_('Code'), max_length=100, unique=True, null=True, blank=True, db_index=True)
     
     def __str__(self):
         return self.code
